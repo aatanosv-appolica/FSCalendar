a0b92ec7e391f2042c17e0c3d427e68c68872f96
Atanas Atanasov <aatanasov@appolica.com>
2017-01-09 13:31:09 +0200

    Change and sticky Header title to left alignment

--- FSCalendar/FSCalendarStickyHeader.m
+++ FSCalendar/FSCalendarStickyHeader.m
@@ -88,6 +88,7 @@
 
 - (void)configureAppearance
 {
+    _titleLabel.textAlignment = NSTextAlignmentLeft;
     _titleLabel.font = self.calendar.appearance.headerTitleFont;
     _titleLabel.textColor = self.calendar.appearance.headerTitleColor;
     [self.weekdayView configureAppearance];
